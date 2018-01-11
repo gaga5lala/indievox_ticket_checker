2e8224bf3998b1808f14e6514024d1d2645e46b9
gaga5lala <gaga5lala1013@gmail.com>
2018-01-11 21:12:42 +0800

    fix typo

--- notifier/telegram.rb
+++ notifier/telegram.rb
@@ -7,7 +7,7 @@ module Notifier
   class Telegram
     def self.create(h)
       bot = ::Telegram::Bot::Client.new(ENV["TELEGRAM_TOKEN"])
-      bot.api.send_message(chat_id: ENV["TELEGRAM_USER_ID"],
+      bot.api.send_message(chat_id: ENV["TELEGRAM_CHAT_ID"],
                            text: h.map{|k,v| "#{k}: #{v}"}.join(IFS))
     end
   end
